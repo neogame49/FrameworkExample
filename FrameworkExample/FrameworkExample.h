89885c7817e7ad15a7dcf57e58e4d8629c6066aa
Roman Shcherbii <rscher@softserveinc.com>
2016-02-11 12:58:49 +0200

    initial commit

---
+++ FrameworkExample/FrameworkExample.h
@@ -0,0 +1,19 @@
+//
+//  FrameworkExample.h
+//  FrameworkExample
+//
+//  Created by Roman Shcherbii on 2/11/16.
+//  Copyright © 2016 Roman Shcherbii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FrameworkExample.
+FOUNDATION_EXPORT double FrameworkExampleVersionNumber;
+
+//! Project version string for FrameworkExample.
+FOUNDATION_EXPORT const unsigned char FrameworkExampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkExample/PublicHeader.h>
+
+
---
+++ FrameworkExample/NewFile.swift
@@ -0,0 +1,9 @@
+//
+//  NewFile.swift
+//  FrameworkExample
+//
+//  Created by Roman Shcherbii on 2/11/16.
+//  Copyright © 2016 Roman Shcherbii. All rights reserved.
+//
+
+import Foundation
